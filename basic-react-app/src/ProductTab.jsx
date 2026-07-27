8b1faf52d5e5d51b6b67d15c7ae376e21e894e88
Shaurya <shauryagoel2005@gmail.com>
2026-07-28 01:10:46 +0530

    Product Tab

---
+++ basic-react-app/src/ProductTab.jsx
@@ -0,0 +1,11 @@
+import Product from './Product.jsx'
+
+function ProductTab() {
+    return(
+        <div>
+            <Product />
+            <Product />
+        </div>
+    );
+}
+export default ProductTab;
