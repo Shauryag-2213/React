75350b88a430e5c112e025af5b7af5b0b5095641
Shaurya <shauryagoel2005@gmail.com>
2026-07-28 01:23:56 +0530

    Changes made to Product file

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,6 +1,7 @@
+import './Product.css'
 function Product(){
     return (
-        <div>
+        <div className="Product">
             <h3>Product</h3>
             <h3>Description</h3>
         </div>
