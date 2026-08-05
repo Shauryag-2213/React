7ace547ce75b6c5d3bac7112ca496dafeac03169
Shauryag-2213 <shauryagoel2005@gmail.com>
2026-08-05 17:14:53 +0530

    Like Button Added

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Counter from './Counter'
+import Like from './Like'
 function App() {
 
   return (
     <>
-      <h1>React-States</h1> 
-      <Counter/>
+      <p>React-States</p> 
+      <Like/>
     </>
   )
 }
--- react-state/src/Like.jsx
+++ react-state/src/Like.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 export default function Like(){
+    let [Likes, setLikes] = useState(true);
+    function toggleLike(){
+        setLikes(!Likes);
+        console.log(setLikes)
+    }
+    let likeStyle = {color : "red"};
+    let disStyle = {color : "white"}
     return(<div>
-        <p>Like Button</p>
+           <p onClick={toggleLike}>
+            {Likes ? (
+                <i class="fa-solid fa-thumbs-up" style={likeStyle}>&nbsp;&nbsp;&nbsp;Liked</i>
+            ) : (
+                <i class="fa-solid fa-thumbs-down" style={disStyle}>&nbsp;&nbsp;&nbsp;Disliked</i>
+            )
+            }
+           </p>
     </div>
     );
 }
